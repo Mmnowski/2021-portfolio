aabe50cba20ba351cdc98f86a816f1490fa94031
pawel <pawel.stankiewicz@limebrains.com>
2021-03-22 16:28:02 +0100

    Added styled components + basic theming

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react"
+import { ThemeProvider } from "styled-components";
 import '../styles/globals.css'
+import { lightTheme, darkTheme, GlobalStyles } from "../styles/themeConfig"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState("light")
+  const [isMounted, setIsMounted] = useState(false)
+
+  const toggleTheme = () => {
+      theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles/>
+      { isMounted && <>
+        <button onClick={toggleTheme}>Theme test</button>
+        <Component {...pageProps} />
+      </> }
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
